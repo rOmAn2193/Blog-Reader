e737110ee69c2a1b03dc62864cb55ce420e4c05a
rOmAn2193 <roman_puzey@aol.com>
2016-02-01 14:39:16 -0700

    first commit

--- Blog Reader/MasterViewController.swift
+++ Blog Reader/MasterViewController.swift
@@ -17,11 +17,91 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.leftBarButtonItem = self.editButtonItem()
 
-        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
-        self.navigationItem.rightBarButtonItem = addButton
+        let appDel: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+
+        let context: NSManagedObjectContext = appDel.managedObjectContext
+
+        let url = NSURL(string:"https://www.googleapis.com/blogger/v3/blogs/10861780/posts?key=AIzaSyCXln1FTHeMxAMr13HPcdIOJPV0TrIJUr0")!
+
+        let session = NSURLSession.sharedSession()
+
+        let task = session.dataTaskWithURL(url) { (data, response, error) -> Void in
+
+            if error != nil
+            {
+                print(error)
+            }
+            else
+            {
+                if let data = data
+                {
+                    //print(NSString(data: data, encoding: NSUTF8StringEncoding))
+
+                    do
+                    {
+                        let jsonResult = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+
+                        if jsonResult.count > 0
+                        {
+                            if let items = jsonResult["items"] as? NSArray
+                            {
+                                let request = NSFetchRequest(entityName: "BlogItems")
+
+                                request.returnsObjectsAsFaults = false
+
+                                do
+                                {
+                                    let results = try context.executeFetchRequest(request)
+
+                                    if results.count > 0
+                                    {
+                                        for result in results
+                                        {
+                                            context.deleteObject(result as! NSManagedObject)
+
+                                            do
+                                            {
+                                                try context.save()
+                                            }
+                                            catch
+                                            {
+                                                print("Delete object error")
+                                            }
+                                        }
+                                    }
+                                }
+                                catch
+                                {
+                                    print("Request error")
+                                }
+
+                                for item in items
+                                {
+                                if let title = item["title"] as? String
+                                {
+                                    if let content = item["content"] as? String
+                                    {
+                                        let newPost: NSManagedObject = NSEntityDescription.insertNewObjectForEntityForName("BlogItems", inManagedObjectContext: context)
+
+                                        newPost.setValue(title, forKey: "title")
+                                        newPost.setValue(content, forKey: "content")
+                                    }
+                                }
+                              }
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        print("There is an error")
+                    }
+                }
+            }
+        }
+
+        task.resume()
+
         if let split = self.splitViewController {
             let controllers = split.viewControllers
             self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
@@ -36,26 +116,6 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-    func insertNewObject(sender: AnyObject) {
-        let context = self.fetchedResultsController.managedObjectContext
-        let entity = self.fetchedResultsController.fetchRequest.entity!
-        let newManagedObject = NSEntityDescription.insertNewObjectForEntityForName(entity.name!, inManagedObjectContext: context)
-             
-        // If appropriate, configure the new managed object.
-        // Normally you should use accessor methods, but using KVC here avoids the need to add a custom class to the template.
-        newManagedObject.setValue(NSDate(), forKey: "timeStamp")
-             
-        // Save the context.
-        do {
-            try context.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            //print("Unresolved error \(error), \(error.userInfo)")
-            abort()
-        }
     }
 
     // MARK: - Segues
@@ -112,7 +172,7 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
 
     func configureCell(cell: UITableViewCell, atIndexPath indexPath: NSIndexPath) {
         let object = self.fetchedResultsController.objectAtIndexPath(indexPath)
-        cell.textLabel!.text = object.valueForKey("timeStamp")!.description
+        cell.textLabel!.text = object.valueForKey("title")!.description
     }
 
     // MARK: - Fetched results controller
@@ -124,14 +184,14 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
         
         let fetchRequest = NSFetchRequest()
         // Edit the entity name as appropriate.
-        let entity = NSEntityDescription.entityForName("Event", inManagedObjectContext: self.managedObjectContext!)
+        let entity = NSEntityDescription.entityForName("BlogItems", inManagedObjectContext: self.managedObjectContext!)
         fetchRequest.entity = entity
         
         // Set the batch size to a suitable number.
         fetchRequest.fetchBatchSize = 20
         
         // Edit the sort key as appropriate.
-        let sortDescriptor = NSSortDescriptor(key: "timeStamp", ascending: false)
+        let sortDescriptor = NSSortDescriptor(key: "title", ascending: false)
         
         fetchRequest.sortDescriptors = [sortDescriptor]
         
